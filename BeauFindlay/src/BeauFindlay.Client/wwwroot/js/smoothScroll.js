4fa27ea626255ffcff5221c7bad4694de941f474
Beau Findlay <me@beaufindlay.com>
2024-03-10 21:42:32 +0000

    // WIP

---
+++ BeauFindlay/src/BeauFindlay.Client/wwwroot/js/smoothScroll.js
@@ -0,0 +1,10 @@
+﻿function scrollToElement(id) {
+    const element = document.getElementById(id);
+    if (element instanceof HTMLElement) {
+        element.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+            inline: "nearest"
+        });
+    }
+}
