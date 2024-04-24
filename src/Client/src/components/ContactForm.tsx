3c2edea9e06e7e5c4376a08a37723bf599b53335
Beau Findlay <me@beaufindlay.com>
2024-04-24 21:55:51 +0100

    Add contact form

---
+++ src/Client/src/components/ContactForm.tsx
@@ -0,0 +1,58 @@
+import { FaRegPaperPlane } from "react-icons/fa6";
+import Link from "./Link";
+import TextInput from "./TextInput";
+import TextAreaInput from "./TextAreaInput";
+import Button from "./Button";
+import Label from "./Label";
+
+export default function ContactForm() {
+  return (
+    <div className="mx-auto max-w-xl py-4">
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          console.log("Submitted");
+        }}
+      >
+        <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+          <div className="sm:col-span-2">
+            <Label htmlFor="name">Name</Label>
+            <div className="mt-2.5">
+              <TextInput id="name" type="text" />
+            </div>
+          </div>
+          <div className="sm:col-span-2">
+            <Label htmlFor="email">Email</Label>
+            <div className="mt-2.5">
+              <TextInput id="email" type="email" />
+            </div>
+          </div>
+          <div className="sm:col-span-2">
+            <Label htmlFor="message">Message</Label>
+            <div className="mt-2.5">
+              <TextAreaInput id="message" />
+            </div>
+          </div>
+        </div>
+        <div className="mt-4">
+          <small>
+            This site is protected by reCAPTCHA and the Google{" "}
+            <Link href="https://policies.google.com/privacy" target="_blank">
+              Privacy Policy
+            </Link>{" "}
+            and{" "}
+            <Link href="https://policies.google.com/terms" target="_blank">
+              Terms of Service
+            </Link>{" "}
+            apply.
+          </small>
+        </div>
+        <div className="mt-4 flex items-center justify-center">
+          <Button type="submit">
+            <FaRegPaperPlane className="mr-2" /> Send
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+}
--- src/Client/src/pages/ContactPage.tsx
+++ src/Client/src/pages/ContactPage.tsx
@@ -1,3 +1,4 @@
+import ContactForm from "../components/ContactForm";
 import Text from "../components/Text";
 import Title from "../components/Title";
 
@@ -5,10 +6,11 @@ export default function ContactPage() {
   return (
     <>
       <Title className="text-center">Contact</Title>
-      <Text className="text-center py-8">
+      <Text className="text-center pb-8">
         If you think I can help with your project or you'd just like to talk
         tech, send me a message!
       </Text>
+      <ContactForm />
     </>
   );
 }
