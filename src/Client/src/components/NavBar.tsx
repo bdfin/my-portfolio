2d5676f5967905885f765a56ecdda72bf1ce9b73
Beau Findlay <beau@unhurd.co.uk>
2024-04-24 14:33:27 +0100

    Add footer and replace heroicons with react-icons

--- src/Client/src/components/NavBar.tsx
+++ src/Client/src/components/NavBar.tsx
@@ -1,16 +1,17 @@
 import { Dialog, Popover } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { useState } from "react";
+import { FaBars, FaXmark } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
 import logo from "../assets/logo.webp";
+
 
 export default function NavBar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="">
+    <header className="pt-6">
       <nav
-        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        className="mx-auto flex max-w-7xl items-center justify-between"
         aria-label="Global"
       >
         <div className="flex lg:flex-1">
@@ -26,7 +27,7 @@ export default function NavBar() {
             onClick={() => setMobileMenuOpen(true)}
           >
             <span className="sr-only">Open main menu</span>
-            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            <FaBars className="h-6 w-6" aria-hidden="true" />
           </button>
         </div>
         <Popover.Group className="hidden lg:flex lg:gap-x-12">
@@ -57,7 +58,7 @@ export default function NavBar() {
               onClick={() => setMobileMenuOpen(false)}
             >
               <span className="sr-only">Close menu</span>
-              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+              <FaXmark className="h-6 w-6" aria-hidden="true" />
             </button>
           </div>
           <div className="mt-6 flow-root">
--- src/Client/src/pages/Layout.tsx
+++ src/Client/src/pages/Layout.tsx
@@ -1,12 +1,17 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 export default function Layout() {
   return (
-    <div className="bg-black font-mono text-slate-50 min-h-screen antialiased">
-      <NavBar />
-      <div className="flex flex-col min-h-screen mx-auto max-w-7xl fade-in p-6 lg:px-8">
-        <Outlet />
+    <div className="bg-black font-mono text-slate-50 antialiased px-6 lg:px-8">
+      <div className="flex flex-col min-h-screen mx-auto max-w-7xl fade-in ">
+        <NavBar />
+        <div className="flex-1">
+          <Outlet />
+        </div>
+
+        <Footer />
       </div>
     </div>
   );
