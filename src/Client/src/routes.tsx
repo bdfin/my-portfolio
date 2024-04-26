9991cd1bf7cc82dc956320f7f64a71ab8be4de68
Beau Findlay <beau@unhurd.co.uk>
2024-04-26 18:06:21 +0100

    Add error page

--- src/Client/src/routes.tsx
+++ src/Client/src/routes.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
+import ErrorPage from "./pages/ErrorPage";
 
 const HomePage = React.lazy(() => import("./pages/HomePage"));
 const ContactPage = React.lazy(() => import("./pages/ContactPage"));
@@ -10,6 +11,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       { path: "contact", element: <ContactPage /> },
